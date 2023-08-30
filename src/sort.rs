048f6b2747ad335ec80c31e11c772c7f3555de62
chomosuke <a13323600@gmail.com>
2023-08-30 19:27:37 +1000

    formatted, deleted orginial function, removed nested unsafe and safety comment

--- src/sort.rs
+++ src/sort.rs
@@ -1,29 +1,10 @@
 use std::{collections::VecDeque, ptr};
 
-pub fn merge_sort<T>(mut vec: Vec<T>) -> Vec<T>
-where
-    T: PartialOrd,
-{
-    let mut size = 1;
-    while size < vec.len() {
-        for i in 0..=vec.len() / size / 2 {
-            let i1 = i * 2 * size;
-            let i2 = (i * 2 + 1) * size;
-            let i2 = i2.min(vec.len());
-            let i3 = (i + 1) * 2 * size; // this is guarantteed to be smaller or equals to vec.len()
-            let i3 = i3.min(vec.len());
-            merge(&mut vec[i1..i3], i2 - i1);
-        }
-        size *= 2;
-    }
-    vec
-}
-
-trait MergeSort {
+pub trait MergeSort {
     fn merge_sort(&mut self);
 }
 
-impl <T> MergeSort for Vec<T>
+impl<T> MergeSort for Vec<T>
 where
     T: PartialOrd,
 {
@@ -33,19 +14,20 @@ where
     }
 }
 
-impl <T> MergeSort for [T] 
+impl<T> MergeSort for [T]
 where
     T: PartialOrd,
 {
     #[inline]
     fn merge_sort(&mut self) {
-        split_and_merge(self)
+        merge_sort(self)
     }
 }
 
-fn split_and_merge<T>(slice: &mut [T])
+fn merge_sort<T>(slice: &mut [T])
 where
-    T: PartialOrd, {
+    T: PartialOrd,
+{
     let mut size = 1;
     while size < slice.len() {
         for i in 0..=slice.len() / size / 2 {
@@ -60,15 +42,13 @@ where
     }
 }
 
-fn merge<T>(slice: &mut [T], mid: usize) 
+fn merge<T>(slice: &mut [T], mid: usize)
 where
-    T: PartialOrd
+    T: PartialOrd,
 {
     let mut s = slice.as_mut_ptr();
 
-    let mut v1 = unsafe { 
-        fill_into_vec(&slice, mid) 
-    };
+    let mut v1 = unsafe { fill_into_vec(&slice, mid) };
 
     let mut s1 = v1.as_ptr();
 
@@ -80,9 +60,11 @@ where
         )
     };
 
+    //SAFETY: this is always indexing into known size
     unsafe {
         while s1 != e1 && s2 != e2 {
             if *s1 < *s2 {
+                // SAFETY: ptr::copy() does not drop the value being over written
                 ptr::copy(s1, s, 1);
                 s1 = s1.add(1);
             } else {
@@ -92,35 +74,23 @@ where
             s = s.add(1);
         }
         if s1 != e1 {
-                ptr::copy(s1, s, e1.offset_from(s1) as usize);
+            ptr::copy(s1, s, e1.offset_from(s1) as usize);
         }
-            v1.set_len(0);
+        // SAFETY: set len to zero so that the content does not get dropped.
+        v1.set_len(0);
+        // SAFETY: nothing of value T gets dropped here and if my logic is correct nothing will be
+        // leaked.
     }
 }
 
-unsafe fn fill_into_vec<T>(slice: &[T], size: usize) -> Vec<T> 
+unsafe fn fill_into_vec<T>(slice: &[T], size: usize) -> Vec<T>
 where
-    T: PartialOrd
+    T: PartialOrd,
 {
     let mut vec = Vec::<T>::with_capacity(size);
-
-    unsafe {
-        ptr::copy(slice.as_ptr(), vec.as_mut_ptr(), size);
-        vec.set_len(size);
-    }
+    ptr::copy(slice.as_ptr(), vec.as_mut_ptr(), size);
+    vec.set_len(size);
     vec
-}
-
-#[test]
-fn test_merge_sort() {
-    let vec: Vec<u32> = vec![
-        1, 54, 618, 45, 32, 35, 6480, 573, 18, 816, 31, 21, 0, 789, 645, 321, 591, 64, 28,
-    ];
-    let mut sorted = vec.clone();
-    sorted.sort();
-    let vec = merge_sort(vec);
-
-    assert_eq!(sorted, vec);
 }
 
 #[test]
@@ -129,7 +99,7 @@ fn test_merge_sort_self_slice() {
         1, 54, 618, 45, 32, 35, 6480, 573, 18, 816, 31, 21, 0, 789, 645, 321, 591, 64, 28,
     ];
     let mut vec_2 = vec.clone();
-    let slice_1 : &mut [u16] = vec.as_mut_slice();
+    let slice_1: &mut [u16] = vec.as_mut_slice();
     let slice_2: &mut [u16] = vec_2.as_mut_slice();
 
     slice_1.sort();
@@ -139,7 +109,7 @@ fn test_merge_sort_self_slice() {
 }
 
 #[test]
-fn merge_sort_self_vec() {
+fn test_merge_sort_self_vec() {
     let mut vec: Vec<u16> = vec![
         1, 54, 618, 45, 32, 35, 6480, 573, 18, 816, 31, 21, 0, 789, 645, 321, 591, 64, 28,
     ];
